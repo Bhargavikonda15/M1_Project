d760f2d74d01d2c1af0383a4d4038393ae9f95ca
Bhargavikonda15 <93070074+Bhargavikonda15@users.noreply.github.com>
2022-03-31 14:13:49 +0530

    Add files via upload

---
+++ 3_Implementation/Inc/Main_online_banking_system.h
@@ -0,0 +1,13 @@
+#ifndef __Online_banking_system_H__
+#define __Online_banking_system_H__
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+/*struct user 
+{
+	char phone[50];
+	char ac[50];
+	char password[50];
+	float balance;
+};*/
+#endif
